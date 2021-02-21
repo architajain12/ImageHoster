6b9cd9489f618b2a240ab586d1f0413f2821ec90
unknown <jainarchita12@gmail.com>
2021-02-21 14:38:05 +0530

    Only owner can edit post issue fixed

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.*;
@@ -92,13 +92,22 @@ public class ImageController {
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, Model model, HttpSession session, RedirectAttributes redirect) {
         Image image = imageService.getImage(imageId);
-
-        String tags = convertTagsToString(image.getTags());
-        model.addAttribute("image", image);
-        model.addAttribute("tags", tags);
-        return "images/edit";
+        User imgOwner = image.getUser();
+        User loggedInUser = (User) session.getAttribute("loggeduser");
+        if(!imgOwner.getUsername().equalsIgnoreCase(loggedInUser.getUsername())){
+            String error = "Only the owner of the image can edit the image";
+            String imageTitle = image.getTitle();
+            redirect.addAttribute("editError", error).addFlashAttribute("editError", error);
+            return "redirect:images/"+imageId+"/"+imageTitle;
+        }
+        else {
+            String tags = convertTagsToString(image.getTags());
+            model.addAttribute("image", image);
+            model.addAttribute("tags", tags);
+            return "images/edit";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'images/edit' and also the incoming request is of PUT type
